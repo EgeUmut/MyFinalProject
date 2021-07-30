750114456122275b26d1fe9668d971e6166d17be
EgeUmut <egeumuttali@hotmail.com>
2021-07-30 10:40:46 +0300

    Add project files.

---
+++ Kurs5/GamerManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs5
+{
+    //MicroService
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayıt Olundu!");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Başarısız!");
+            }
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayıt Silindi!!");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Silme Başarısız!");
+            }
+        }
+
+        public void Update(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer) == true)
+            {
+                Console.WriteLine("Kayıt Güncellendi!");
+            }
+            else
+            {
+                Console.WriteLine("Kayıt Güncelleme Başarısız!");
+            }
+        }
+
+    }
+}
+
+
+
---
+++ Kurs5/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs5
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if(gamer.BirthYear == 2000 && gamer.Name == "Ege"
+                && gamer.LastName == "Tali" && gamer.IdentityNumber == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
