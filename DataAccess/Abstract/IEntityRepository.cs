b06b45ce62b8722024509de2421d1c5d3ccd512a
EgeUmut <egeumuttali@hotmail.com>
2021-08-01 17:20:37 +0300

    kurs-5 ayrı diğerleri kurs-6 1 ağustos 2021

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // T ye gelebilecek değerleri kısıtlamak için generic constraint ekliyoruz
+    // where T:class referans tip olabilir demek , IEntity yazarsan sadece IEntity referansı olan classları kabul eder
+    //Yani IEntity olabilir ya da IEntity referans eden bir nesne olabilir
+    // new() :  sadece newlenebilen referanslar kabul edilebilir.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T,bool>> filter);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
