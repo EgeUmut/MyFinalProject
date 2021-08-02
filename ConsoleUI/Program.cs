ad324408752e6266f911d98d09217bec77dc5913
EgeUmut <egeumuttali@hotmail.com>
2021-08-02 14:47:53 +0300

    02/08/2021 öğleden sonra

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,21 +11,32 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            /* product manager testi
-            foreach (var product in productManager.GetByUnitPrice(50,200))
-            {
-                Console.WriteLine(product.CategoryId + "  " + product.ProductName + " \t " + product.UnitPrice + " $");
-            }*/
 
             // ProductTest(productManager);
 
+            //CategoryTest(categoryManager);
+
+            //viewTest(productManager);
+
+        }
+
+        private static void viewTest(ProductManager productManager)
+        {
+            foreach (var view in productManager.GetProductDetails())
+            {   // biraz çorba ama anlarsın
+                Console.WriteLine("product id: " + view.ProductId +
+                    "\t" + view.ProductName +
+                    "\t in stock: " + view.UnitsInStock +
+                    "\t category name: " + view.CategoryName);
+            }
+        }
+
+        private static void CategoryTest(CategoryManager categoryManager)
+        {
             foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(category.CategoryId+" "+category.CategoryName);
+                Console.WriteLine(category.CategoryId + " " + category.CategoryName);
             }
-
-
-
         }
 
         private static void ProductTest(ProductManager productManager)
